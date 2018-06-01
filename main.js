172b19e61f329a91aa8ca22ab57762cf7f8b4f6d
taehyunlim <taehyun.lim@gmail.com>
2018-06-01 16:12:29 -0700

    Rates selection to show carrier info

--- main.js
+++ main.js
@@ -6,18 +6,39 @@ const api = new EasyPost(configs.apiKey);
 
 // Hardcoded string vars
 const mainManu = ['address', 'shipment', 'tracker', 'parcel'];
-const addressInput = [{field:'name', default: 'Tae Home'}, {field: 'street1', default: '5404 Heatrland Drive'}, {field: 'street2', default: ''}, {field: 'city', default: 'San Ramon'}, {field: 'state', default: 'CA'}, {field: 'zip', default: '94582'}, {field: 'country', default: 'USA'}, {field: 'phone', default: '510-432-5656'}];
-const parcelInput = [{field: 'length', default: 30}, {field: 'width', default: 30}, {field: 'height', default: 30}, {field: 'weight', default: 30}];
+const addressInput = [
+  {field:'name', default: 'Tae Home'},
+  {field: 'street1', default: '5404 Heatrland Drive'},
+  {field: 'street2', default: ''},
+  {field: 'city', default: 'San Ramon'},
+  {field: 'state', default: 'CA'},
+  {field: 'zip', default: '94582'},
+  {field: 'country', default: 'USA'},
+  {field: 'phone', default: '510-432-5656'}
+];
+const parcelInput = [
+  {field: 'length', default: 15},
+  {field: 'width', default: 15},
+  {field: 'height', default: 15},
+  {field: 'weight', default: 15}
+];
+const carrierAccounts = [
+  {name: 'USPS (EP Support)', id: 'ca_c08beb7d8abf458a8bba7ac3742439cb'},
+  {name: 'UPS (EP Support)', id: 'ca_e35cc2f45bf743b899a580caf45a6063'},
+  {name: 'FedEx (Tae)', id: 'ca_a92788837eb54764b6cf3fe79f86748c'},
+  {name: 'UPS (Tae)', id: 'ca_921780b836c14100bdc991261d9fca6e'},
+];
 const addrArray = [
-  {name: "Tae Home", id: "adr_43768bb693804629bad3e2f8190e4a47"},
-  {name: "Tae EP", id: "adr_5cf8cddb7cc545799abf66690ec4beee"},
-  {name: "", id: ""}
+  {name: 'Tae Home', id: 'adr_43768bb693804629bad3e2f8190e4a47'},
+  {name: 'Tae EP', id: 'adr_5cf8cddb7cc545799abf66690ec4beee'},
+  {name: 'Canada Address 1', id: 'adr_c2e38e2d67b74adbb8ac3b073e80dce5'}
 ];
 const parcelArray = [
-  {name: "30/30/30/30", id: "prcl_1fc93fe8bd5c4a2885944af6156b18a5"},
+  {name: '30/30/30/30', id: 'prcl_1fc93fe8bd5c4a2885944af6156b18a5'},
+  {name: '10/10/10/1', id: 'prcl_c0947ff787ad4ccc837107a975a71675'},
 ];
 const trackerArray = [
-  {name: "Test Tracker", id: "trk_370effe60fc3434787915b3521988e73"}
+  {name: 'Test Tracker', id: 'trk_370effe60fc3434787915b3521988e73'}
 ];
 
 //  Declare empty vars globally
@@ -37,7 +58,6 @@ const inputObject = (inputField, inputDefault, title) => {
 }
 
 const inputConfirm = (body, inputArray) => {
-  console.log(body);
   return new Promise((resolve, reject) => {
     inquirer.prompt([{
       type: 'confirm',
@@ -74,7 +94,7 @@ const listPrompt = (array, title) => {
     }).then(answers => {
       if (title === 'service') {
         let result = rates.find(e => e.handle === answers[title]);
-        console.log(result);
+        // console.log(result);
         resolve(result);
       } else {
         let result = array.find(e => e.name === answers[title]);
@@ -87,8 +107,7 @@ const listPrompt = (array, title) => {
 function addr() {
   currentMenu = 'address'
   return new Promise((resolve, reject) => {
-    // Prompt for fromAddress
-    inputPrompt(addressInput, "address")
+    inputPrompt(addressInput, 'address')
     .then(body => {
       body['verify'] = ['delivery'];
       let address = new api.Address(body);
@@ -112,7 +131,7 @@ function addr() {
 function prc() {
   currentMenu = 'parcel'
   return new Promise((resolve, reject) => {
-    inputPrompt(parcelInput, "parcel")
+    inputPrompt(parcelInput, 'parcel')
     .then(body => {
       // parseInt parcel dimension & weight inputs
       Object.keys(body).map((key, index) => {
@@ -145,20 +164,23 @@ function shp() {
       shpInput['parcel'] = { id: resultsArray[2]['id'] };
       let shipment = new api.Shipment(shpInput);
       return shipment.save();
-      //resolve(shpInput);
     }).then(shp => {
       shpId = shp.id;
       shp.rates.forEach(rate => {
+        let acctName = carrierAcctName(rate);
         rates.push({
-          "carrier": rate.carrier,
-          "service": rate.service,
-          "rate": rate.rate,
-          "id": rate.id,
-          "handle": `${rate.carrier}-${rate.service}: ${rate.rate}`
+          'accountName': acctName,
+          'carrier': rate.carrier,
+          'service': rate.service,
+          'rate': rate.rate,
+          'id': rate.id,
+          'handle': `${acctName}-${rate.carrier}-${rate.service}: ${rate.rate}`
         })
       })
       let services = shp.rates.map(rate => {
-        return `${rate.carrier}-${rate.service}: ${rate.rate}`
+        // Create services handles
+        let acctName = carrierAcctName(rate);
+        return `${acctName}-${rate.carrier}-${rate.service}: ${rate.rate}`
       })
       return listPrompt(services, 'service');
     }).then(result => {
@@ -167,12 +189,24 @@ function shp() {
     }).then(rshp => {
       return rshp.buy(rateId);
     }).then(res => {
+      // console.log(res);
+      opn(res.postage_label.label_url);
       opn(res.tracker.public_url);
       resolve(res.tracker.id);
     }).catch(e => { if (e) throw e })
 
+    // Find carrier account name from the global array and return carrier_account_id if not found
+    function carrierAcctName(rate) {
+      for (let i = 0; i < carrierAccounts.length; i++) {
+        if (carrierAccounts[i].id === rate.carrier_account_id) { return carrierAccounts[i].name; }
+      }
+      return rate.carrier_account_id
+    }
+
   });
 }
+
+
 
 function main() {
   inquirer.prompt([{
@@ -198,7 +232,7 @@ function main() {
       type: 'list',
       name: 'exit',
       choices: ["Return to main menu", "Exit"],
-      message: "Exit or stay?"
+      message: "[main] Exit or stay?"
     }]);
   }).then(answers => {
     if (answers.exit === 'Exit') { process.exit() }
